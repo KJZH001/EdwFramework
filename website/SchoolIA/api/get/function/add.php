10fbf43466b78880d6cf561cdcb85c646a52866f
KJZH001 <2802007619@qq.com>
2021-06-04 17:47:55 +0800

    [更新]添加新增维修记录api

--- website/SchoolIA/api/get/function/add.php
+++ website/SchoolIA/api/get/function/add.php
@@ -1,6 +1,51 @@
 <?php
 
-// INSERT INTO `weixiu` (`id`, `title`, `contant`, `time`, `status`, `evaluate`) 
-// VALUES (NULL, '第三条维修记录', '这是一条满分的已完成维修记录', CURRENT_TIMESTAMP, '1', '2');
+//$=$_REQUEST[""];
+$title=$_REQUEST["title"];
+$contant=$_REQUEST["api-contant"];
+
+if($gb_debug=="true")
+{
+    echo "调用了add.php<br>";
+}
+
+//判定数据库
+if ($gb_sqlonly=="false") 
+{
+$gb_sqlname=$gb_sqlprefix."schoolia";
+}
+//引入数据库
+require("./system/core/conn.php");
+//查询数据库
+$sql="INSERT INTO `weixiu` (`id`, `title`, `contant`, `time`, `status`, `evaluate`) VALUES (NULL, '".$title."', '".$contant."', CURRENT_TIMESTAMP, '0', '0');";
+echo "<br>".$sql;
+
+if($return="json")
+{
+    if($gb_debug=="false")
+	{
+		header("content-type:application/json");
+	}
+    if (mysqli_query($gb_conn, $sql)) {
+        $result_json=array('msg'=>'新记录插入成功');
+        echo json_encode($result_json);
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($gb_conn);
+    }
+    
+    
+}
+else
+{
+    echo "不支持的数据类型";
+}
+
+
+
+
+//关闭数据库
+mysqli_close($gb_conn);
+
+
 
 ?>
--- website/SchoolIA/api/get/function/list-all.php
+++ website/SchoolIA/api/get/function/list-all.php
@@ -15,32 +15,31 @@ require("./system/core/conn.php");
 $sql="select * from weixiu order by id asc;";
 $result=mysqli_query($gb_conn,$sql);
 
+$num=1;
+$result_json=null;
+
 //echo "id title contant time state（状态） evaluate（评分)<br>";
 while($row=mysqli_fetch_assoc($result))
 {
-$id=$row['id'];
-$title=$row['title'];
-$contant=$row['contant'];
-$time=$row['time'];
-$state=$row['status'];
-$evaluate=$row['evaluate'];
 
-// $remark=str_replace(PHP_EOL,"<br>", $remark);
-// $object=str_replace(PHP_EOL,"<br>", $object);
-//echo $id." ".$title." ".$contant." ".$time." ".$state." ".$evaluate."<br>";
+$result_json[$num]=$row;
+$num++;
+
+}
+
 if($return="json")
 {
     if($gb_debug=="false")
 	{
 		header("content-type:application/json");
 	}
-    echo json_encode($row);
+    echo json_encode($result_json);
 }
 else
 {
     echo "不受支持的返回值类型";
 }
-}
+
 //关闭数据库
 mysqli_close($gb_conn);
 ?>
