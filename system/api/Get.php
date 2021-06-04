ed084d9c0d779b86e66f8d1b3c390a08dcf8a1a1
KJZH001 <2802007619@qq.com>
2021-06-04 12:26:13 +0800

    [更新]添加调用内部的网站接口api方法

--- system/api/Get.php
+++ system/api/Get.php
@@ -1,16 +1,26 @@
 <?php
 $server=$_REQUEST["server"];
+@$website=$_REQUEST["website"];
 if(file_exists("./system/api/get/".$server.".php"))
-		{
-			require("./system/api/get/".$server.".php");
-		}
-		else
-		{
-			if($gb_debug=="true")
-			{
-				echo "未找到文件";
-			}
-			require("./system/core/404.php");
-		}
+{
+	require("./system/api/get/".$server.".php");
+}
+elseif(file_exists("./website/."$website"./api/Main.php"))
+{
+	if($gb_debug=="true")
+	{
+		echo "尝试调用网站接口"."./website/."$website"./api/Main.php";
+	}
+	require("./website/."$website"./api/Main.php");
+}
+else
+{
+	if($gb_debug=="true")
+	{
+		echo "未找到文件";
+	}
+	require("./system/core/404.php");
+}
 
+//http://dj.edw.moeworld.tech/api.php/type=get&website=Schoolia&server=list
 ?>
--- website/SchoolIA/index/weixiu.php
+++ website/SchoolIA/index/weixiu.php
@@ -1,67 +1,33 @@
-<div class="mdui-typo">
-<h1> 创新实验室 <small> 物品登记表 </small></h1>
-</div>
-
-<div class="mdui-table-fluid">
-  <table class="mdui-table">
-    <thead>
-      <tr>
-        <th>#</th>
-        <th>物品</th>
-        <th>参考日期</th>
-        <th>状态</th>
-        <th>备注</th>
-        <th>负责人</th>
-      </tr>
-    </thead>
-    <tbody>
-
-        <?php
-        //判定数据库
+<?php
+//判定数据库
 if ($gb_sqlonly=="false") 
 {
-	$gb_sqlname=$gb_sqlprefix."schoolia";
+$gb_sqlname=$gb_sqlprefix."schoolia";
 }
 //引入数据库
 require("./system/core/conn.php");
 //查询数据库
 //$sql="select * from post order by id desc limit 1;";
-$sql="select * from maker order by id asc;";
+$sql="select * from weixiu order by id asc;";
 $result=mysqli_query($gb_conn,$sql);
+
+echo "id title contant time state（状态） evaluate（评分)<br>";
+while($row=mysqli_fetch_assoc($result))
+{
+$id=$row['id'];
+$title=$row['title'];
+$contant=$row['contant'];
+$time=$row['time'];
+$state=$row['status'];
+$evaluate=$row['evaluate'];
+
+// $remark=str_replace(PHP_EOL,"<br>", $remark);
+// $object=str_replace(PHP_EOL,"<br>", $object);
+echo $id." ".$title." ".$contant." ".$time." ".$state." ".$evaluate."<br>";
+
+//header("content-type:application/json");
+//echo json_encode($row);
+}
+//关闭数据库
+mysqli_close($gb_conn);
 ?>
-
-
-<div class="mdui-col-md-9 mdui-col-xs-12">
-
-	<?php
-	while($row=mysqli_fetch_assoc($result))
-	{
-	$id=$row['id'];
-	$goods=$row['goods'];
-    $time=$row['time'];
-    $state=$row['state'];
-    $remarks=$row['remarks'];
-    $object=$row['object'];
-
-	$remark=str_replace(PHP_EOL,"<br>", $remark);
-    $object=str_replace(PHP_EOL,"<br>", $object);
-	
-		echo "
-			<tr>
-            <td>".$id."</td>
-            <td>".$goods."</td>
-            <td>".$time."</td>
-            <td>".$state."</td>
-            <td><pre>".$remarks."</pre></td>
-            <td><pre>".$object."</pre></td>
-            </tr>
-		";
-	}
-    //关闭数据库
-    mysqli_close($gb_conn);
-    ?>
-
-    </tbody>
-  </table>
-</div>
-
